af1439f2cff51c11374049184cca734cab09fcf8
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2023-03-24 13:53:51 +0000

    include bytes

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 // Example custom build script.
-use copy_to_output::copy_to_output;
-use std::{env, process::Command};
+use std::process::Command;
 
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
@@ -10,12 +9,4 @@ fn main() {
         .arg("build-actors")
         .output()
         .expect("failed to execute process");
-
-    let build_type = format!("{}/deps", &env::var("PROFILE").unwrap());
-
-    copy_to_output(
-        "builtin-actors/output/builtin-actors-mainnet.car",
-        &build_type,
-    )
-    .expect("Could not copy");
 }
