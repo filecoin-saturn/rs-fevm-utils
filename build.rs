3869026e4968a5b87b7d373041d2b0ac25d60d6d
dante <45801863+alexander-camuto@users.noreply.github.com>
2023-03-30 11:17:39 +0100

    fix: recursive submodule init  (#10)

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
-// Example custom build script.
+//! build.rs
+//
 use std::process::Command;
+
+use std::{fs, io};
 
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
@@ -9,4 +12,23 @@ fn main() {
         .arg("build-actors")
         .output()
         .expect("failed to execute process");
+    //
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
+    fs::create_dir_all(&format!("{}/builtin-actors/output", out_dir))
+        .expect("unable to create actors directory");
+
+    let path = format!("builtin-actors/output/builtin-actors-mainnet.car");
+
+    let out_path = format!("{}/{}", out_dir, path);
+
+    let mut out_file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(&out_path)
+        .expect("unable to open/create data file");
+
+    if let Ok(mut source_file) = fs::File::open(&path) {
+        io::copy(&mut source_file, &mut out_file).expect("failed to copy data after opening");
+    }
 }
